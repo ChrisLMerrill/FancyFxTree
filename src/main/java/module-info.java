643556e2607aad1122b19b46c464761572d0c191
Chris <ChrisLMerrill@users.noreply.github.com>
2019-09-12 15:39:20 -0400

    remove module-info

--- src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module FancyFxTree {
-    requires javafx.controls;
-    opens net.christophermerrill.FancyFxTree to javafx.graphics;
-    opens net.christophermerrill.FancyFxTree.example to javafx.graphics;
-}
